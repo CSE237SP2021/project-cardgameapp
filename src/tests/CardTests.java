29326181d1758a39b9e548babec72374eeadeeb5
andrewhuang427 <andrewhuang@wustl.edu>
2021-03-29 10:00:47 -0500

    added compareToCard tests

--- src/tests/CardTests.java
+++ src/tests/CardTests.java
@@ -16,5 +16,45 @@ class CardTests {
 		assertEquals(rank, 0);
 		assertEquals(suit, 2);
 	}
-	
+
+	@Test
+	void compareToCardWithLowerRank() {
+		Card c1 = new Card(5,1);
+		Card c2 = new Card(3,2);
+		int comparison = c1.compareToCard(c2);
+		assertEquals(comparison, 1);
+	}
+
+	@Test
+	void compareToCardWithHigherRank() {
+		Card c1 = new Card(3,2);
+		Card c2 = new Card(5,1);
+		int comparison = c1.compareToCard(c2);
+		assertEquals(comparison, -1);
+	}
+
+	@Test 
+	void compareToCardWithSameRankButLowerSuit(){
+		Card c1 = new Card(12,3);
+		Card c2 = new Card(12,2);
+		int comparison = c1.compareToCard(c2);
+		assertEquals(comparison, 1);
+	}
+
+	@Test 
+	void compareToCardWithSameRankButHigherSuit() {
+		Card c1 = new Card(12,1);
+		Card c2 = new Card(12,2);
+		int comparison = c1.compareToCard(c2);
+		assertEquals(comparison, -1);
+	}
+
+	@Test
+	void compareEqualCards() {
+		Card c1 = new Card(12,2);
+		Card c2 = new Card(12,2);
+		int comparison = c1.compareToCard(c2);
+		assertEquals(comparison, 0);
+	}
+
 }
