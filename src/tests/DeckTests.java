7b22350b57a64ee34099035249f170c95556bbb9
mattm0606 <46689249+mattm0606@users.noreply.github.com>
2021-04-13 14:49:02 -0500

    added shuffle test

--- src/tests/DeckTests.java
+++ src/tests/DeckTests.java
@@ -4,11 +4,22 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import app.Deck;
+
 class DeckTests {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void shuffleTest() {
+		Deck d1 = new Deck();
+		Deck d2 = d1;
+		int count = 0;
+		d1.shuffle();
+		for(int i =0; i<52; i++){
+			if(d1.drawCard().compareToCard(d2.drawCard()) != 0 ){
+				count=1;
+			}
+		}
+		assertEquals(count,0);
 	}
 	
 }
