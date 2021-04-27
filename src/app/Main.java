284fbb7620d6ae4ec0fbfea57f132b41ca8dcdb6
root <root@EC2018046.electrocore.local>
2021-04-27 19:40:31 -0400

    added basic functionality to write to a text file

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,8 +1,19 @@
 package app;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+		File file1 = new File("leaderboard.txt");
+		FileWriter fw = new FileWriter(file1);
+		PrintWriter pw = new PrintWriter(fw);
+		pw.println("Line 1");
+		pw.println("Line 1");
+		pw.close();
 		//		Card c = new Card(2, 3);
 		//		Card c2 = new Card(2, 2);
 		//		int comparison = c.compareToCard(c2);
@@ -26,8 +37,8 @@ public class Main {
 //		balance = p.getBalance();
 //		System.out.println(balance);
 		
-		GameMenu g = new GameMenu();
-		g.run();
+		//GameMenu g = new GameMenu();
+		//g.run();
 	}
 
 }
